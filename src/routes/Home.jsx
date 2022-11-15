14a6b97830fa8528ea21e95a57eaf0b478eb9b83
VISHWANATH-1 <vishwanathpandey11@gmail.com>
2022-11-15 21:22:38 +0530

    footer

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
@@ -6,6 +7,7 @@ const Home = () => {
   return <div>
     <Navbar /> 
     <Hero />
+    <Footer />
   </div>;
 };
 
